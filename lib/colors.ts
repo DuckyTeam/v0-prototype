bce7ee813c8d415e90d7d74aa7af18b1571b0390
johan-eilertsen <johan@ducky.eco>
2025-01-20 12:21:31 +0100

    Add files via upload
    Uploaded files from v0 table test. Intended to share with Hollie and test collaborating.

---
+++ lib/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  background: '#f8f8f9',
+  foreground: '#455a64',
+  primary: {
+    DEFAULT: '#0089ab',
+    foreground: '#ffffff',
+  },
+  muted: {
+    DEFAULT: '#ccebee',
+    foreground: '#455a64',
+  },
+  card: {
+    DEFAULT: '#ffffff',
+    foreground: '#455a64',
+  }
+}
+
